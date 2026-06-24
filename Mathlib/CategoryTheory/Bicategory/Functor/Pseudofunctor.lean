a64256ac9efe470e6d366ec32abd8456647cfaf9
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-06-24 18:42:27 +0000

    signature fixes, wave 2

--- Mathlib/CategoryTheory/Bicategory/Functor/Pseudofunctor.lean
+++ Mathlib/CategoryTheory/Bicategory/Functor/Pseudofunctor.lean
@@ -90,6 +90,9 @@ initialize_simps_projections Pseudofunctor (+toPrelaxFunctor, -obj, -map, -map�
 
 namespace Pseudofunctor
 
+#adaptation_note
+/-- `respectTransparency.types true` changes the auto-generated lemmas' signature -/
+set_option backward.isDefEq.respectTransparency.types false in
 attribute [simp, to_app (attr := reassoc)]
   map₂_whisker_left map₂_whisker_right map₂_associator map₂_left_unitor map₂_right_unitor
 
