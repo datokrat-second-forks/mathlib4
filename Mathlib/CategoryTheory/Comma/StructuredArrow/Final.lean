164fe75ec26348cb97e71f3e6f411e54d87b2289
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-09-03 12:09:07 +0000

    address warnings

--- Mathlib/CategoryTheory/Comma/StructuredArrow/Final.lean
+++ Mathlib/CategoryTheory/Comma/StructuredArrow/Final.lean
@@ -59,8 +59,8 @@ private lemma final_of_final_costructuredArrowToOver_small (L : A ⥤ T) (R : B 
             Final.colimitIso _ _
       _ ≅ colimit G := (colimitIsoColimitGrothendieck (𝟭 T) G).symm
   convert! Iso.isIso_hom i
-  simp only [Iso.trans_def, comp_obj, grothendieckProj_obj, Grothendieck.pre_obj_base,
-    Grothendieck.pre_obj_fiber, Iso.trans_assoc, Iso.trans_hom, Iso.symm_hom, i]
+  simp only [Iso.trans_def, comp_obj, grothendieckProj_obj, Iso.trans_assoc, Iso.trans_hom,
+    Iso.symm_hom, i]
   rw [← Iso.inv_comp_eq, Iso.eq_inv_comp]
   apply colimit.hom_ext (fun _ => by simp)
 
